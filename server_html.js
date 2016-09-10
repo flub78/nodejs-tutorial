ff7e1f1d6714f1475b1f1223f27d635a0fbc43dd
Flub78 <frederic.peignot@free.fr>
2016-09-10 12:35:34 +0200

    Initial commit

---
+++ server_html.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+
+varserver = http.createServer(
+	function (req, res) {
+	  var url_parts = url.parse(req.url, true);
+	  
+	  var name = url_parts.query.name;
+	  
+	  if (name) {
+	  	console.log('Name: ' + name);
+	  	res.writeHead(200, {'Content-Type': 'application/json'});
+	  	res.end(JSON.stringify({message: 'Hello ' + name + '!'}));
+	  } else {
+	    console.log('Noname!');
+	  	res.writeHead(200, {'Content-Type': 'text/html'});
+	  	fs.readFile('hello2.html', function (err,data) {
+	  		res.end(data);
+	  	});
+	  	
+	  }
+	}
+).listen(1337)
